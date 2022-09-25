d81275e488686784f9fcfea6d0051be87cbabc23
urosperisic <uros2907@gmail.com>
2022-09-25 22:21:49 +0200

    v012.

--- scripts/top-nav.js
+++ scripts/top-nav.js
@@ -1,3 +1,17 @@
-document.addEventListener("scroll", function () {
-    document.querySelector(".top-nav").style.backgroundColor = "#0077b6";
-});
+window.onscroll = function () {
+    scrollFunction();
+};
+
+function scrollFunction() {
+    if (document.body.scrollTop > 10 || document.documentElement.scrollTop > 80) {
+        document.getElementById("logo").style.maxWidth = "80%";
+    } else {
+        document.getElementById("logo").style.maxWidth = "90%";
+    }
+
+    if (document.body.scrollTop > 10 || document.documentElement.scrollTop > 80) {
+        document.getElementById("nd").style.backgroundColor = "#064663";
+    } else {
+        document.getElementById("nd").style.backgroundColor = "transparent";
+    }
+}
